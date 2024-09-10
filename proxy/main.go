2a35e9d3c8dae3ec26332f8211e9c1fdf895a31c
Konstantin <kostya.podsh@yandex.ru>
2024-09-10 15:48:00 +0300

    add graceful shutdown

--- proxy/main.go
+++ proxy/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 	_ "net/http/httputil"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/jwtauth"
@@ -54,5 +60,39 @@ func main() {
 		r.Post("/api/address/geocode", handler.GeoCodeHandler)
 	})
 
-	http.ListenAndServe(":8080", r)
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      r, // Здесь должен быть ваш обработчик запросов
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	// Создание канала для получения сигналов остановки
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Запуск сервера в отдельной горутине
+	go func() {
+		log.Println("Starting server...")
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Server error: %v", err)
+		}
+	}()
+
+	// Ожидание сигнала остановки
+	<-sigChan
+
+	// Создание контекста с таймаутом для graceful shutdown
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	// Остановка сервера с использованием graceful shutdown
+	log.Println("Shutting down server...")
+
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server stopped gracefully")
+
 }
